f291d8c7b2cb3b3c3fd28813d01390899746e927
Julian <cfortin@csumb.edu>
2022-10-06 11:06:40 -0700

    Final update postman success

--- cst438_gradebook/src/main/java/com/cst438/Cst438GradebookApplication.java
+++ cst438_gradebook/src/main/java/com/cst438/Cst438GradebookApplication.java
@@ -1,5 +1,4 @@
 package com.cst438;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
--- cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
@@ -3,5 +3,7 @@ package com.cst438.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository <Course, Integer> {
+	
+	
 
 }
